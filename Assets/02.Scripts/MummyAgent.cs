d29f0b581348f0ed71e56770eb2e71820b7355a0
Lee JaeHyun <myleje@gmail.com>
2023-05-25 14:34:18 +0900

    .

--- Assets/02.Scripts/MummyAgent.cs
+++ Assets/02.Scripts/MummyAgent.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
 
 public class MummyAgent : Agent
 {
@@ -17,5 +18,19 @@ public class MummyAgent : Agent
     {
     }
 
+    // 학습(Episode)이 시작될때 마다 호출되는 메소드
+    public override void OnEpisodeBegin()
+    {
+    }
+
+    // 주변환경을 관측 & 브레인에 전달하는 메소드
+    public override void CollectObservations(VectorSensor sensor)
+    {
+    }
+
+    // 브레인(정책:Policy)에게서 전달받은 명령을 실행하는 메소드
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+    }
 
 }
