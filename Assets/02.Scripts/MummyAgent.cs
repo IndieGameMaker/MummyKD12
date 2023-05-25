f5a9c21a1af5d45615f7cc8720c61a9c670d2ee4
Lee JaeHyun <myleje@gmail.com>
2023-05-25 14:28:41 +0900

    Project Initialization

---
+++ Assets/02.Scripts/MummyAgent.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+
+public class MummyAgent : Agent
+{
+    /*
+        1. 주변 환경을 관측(Observation)
+        2. 행동(Action)    
+        3. 보상(Reward)
+    */
+
+    // 초기화 작업을 위한 한번만 호출되는 메소드
+    public override void Initialize()
+    {
+    }
+
+
+}
