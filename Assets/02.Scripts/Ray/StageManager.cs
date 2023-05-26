80790ee3175de5c7d8891aadbf6cc1bdb294dc14
Lee JaeHyun <myleje@gmail.com>
2023-05-26 09:40:25 +0900

    .

--- Assets/02.Scripts/Ray/StageManager.cs
+++ Assets/02.Scripts/Ray/StageManager.cs
@@ -7,6 +7,29 @@ public class StageManager : MonoBehaviour
     [SerializeField] private GameObject goodItem;
     [SerializeField] private GameObject badItem;
 
+    [Range(10, 50)]
+    public int goodItemCount = 30;
+    [Range(10, 50)]
+    public int badItemCount = 20;
+
+    public List<GameObject> goodItemList = new List<GameObject>();
+    public List<GameObject> badItemList = new List<GameObject>();
+
+    public void InitStage()
+    {
+        // GoodItem 생성
+        for (int i = 0; i < goodItemCount; i++)
+        {
+            // 불규칙한 위치(좌표) 생성
+            Vector3 pos = new Vector3(Random.Range(-40.0f, 40.0f)
+                                    , 0.0f
+                                    , Random.Range(-40.0f, 40.0f));
+
+            // 불규칙한 회전 생성
+
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
