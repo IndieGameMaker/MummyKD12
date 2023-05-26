a9956712366b26ca557aaed869b713f2bfe72c8e
Lee JaeHyun <myleje@gmail.com>
2023-05-26 09:52:35 +0900

    .

--- Assets/02.Scripts/Ray/StageManager.cs
+++ Assets/02.Scripts/Ray/StageManager.cs
@@ -21,14 +21,27 @@ public class StageManager : MonoBehaviour
         for (int i = 0; i < goodItemCount; i++)
         {
             // 불규칙한 위치(좌표) 생성
-            Vector3 pos = new Vector3(Random.Range(-40.0f, 40.0f)
+            Vector3 pos = new Vector3(Random.Range(-23.0f, 23.0f)
                                     , 0.0f
-                                    , Random.Range(-40.0f, 40.0f));
+                                    , Random.Range(-23.0f, 23.0f));
 
             // 불규칙한 회전 생성
             Quaternion rot = Quaternion.Euler(Vector3.up * Random.Range(0, 360));
 
-            goodItemList.Add(Instantiate(goodItem, pos, rot, transform));
+            goodItemList.Add(Instantiate(goodItem, transform.position + pos, rot, transform));
+        }
+        // BadItem 생성
+        for (int i = 0; i < badItemCount; i++)
+        {
+            // 불규칙한 위치(좌표) 생성
+            Vector3 pos = new Vector3(Random.Range(-23.0f, 23.0f)
+                                    , 0.0f
+                                    , Random.Range(-23.0f, 23.0f));
+
+            // 불규칙한 회전 생성
+            Quaternion rot = Quaternion.Euler(Vector3.up * Random.Range(0, 360));
+
+            badItemList.Add(Instantiate(badItem, transform.position + pos, rot, transform));
         }
     }
 
@@ -36,11 +49,5 @@ public class StageManager : MonoBehaviour
     void Start()
     {
         InitStage();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
